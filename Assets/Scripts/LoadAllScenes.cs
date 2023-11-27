7834d580dc5621e636bcfbada1e9d0581215d0e6
Lily Haas <haasl@carleton.edu>
2023-11-27 10:37:30 -0600

    fixes #43, #45
    used rowing minigame as a base for the hedeby chest experience to transfer to using the oculus interaction sdk, now the chest is the appropriate size

--- Assets/Scripts/LoadAllScenes.cs
+++ Assets/Scripts/LoadAllScenes.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class LoadAllScenes : MonoBehaviour
 {
+    // this script as written is not ideal for when the project gets put into one app
+    // i have started looking into a way to assign scenes from the editor but have not had any success yet
+    // this is something to look into more before trying to build the project to launch from a main menu
+    public string[] sceneNames;
     // Start is called before the first frame update
     void Start()
     {
+        /*for (int i = 0; i < sceneNames.Length; i++)
+        {
+            SceneManager.LoadScene(sceneNames[i], LoadSceneMode.Additive);
+        }*/
         SceneManager.LoadScene(1, LoadSceneMode.Additive);
     }
 
@@ -15,5 +23,21 @@ public class LoadAllScenes : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Reload()
+    {
+        /*for (int i = 0; i < sceneNames.Length; i++) 
+        {
+            SceneManager.UnloadSceneAsync(sceneNames[i]);
+        }*/
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.UnloadSceneAsync(0);
+        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        /*for (int i = 0; i < sceneNames.Length; i++)
+        {
+            SceneManager.LoadScene(sceneNames[i], LoadSceneMode.Additive);
+        }*/
     }
 }
