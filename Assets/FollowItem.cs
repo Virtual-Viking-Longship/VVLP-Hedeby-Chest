89c6475fed7bc47465db897e5398a05768322b09
Lily Haas <haasl@carleton.edu>
2023-12-07 14:08:13 -0600

    smoother following, adjustable

--- Assets/FollowItem.cs
+++ Assets/FollowItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowItem : MonoBehaviour
 {
     public Transform item;
+    public float smoothSpeed = 0.125f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,8 @@ public class FollowItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = item.transform.position + new Vector3(0,0.05f,-0.05f);
+        Vector3 itemPos = item.transform.position + new Vector3(0,0.05f,-0.05f);
+        Vector3 smoothPos = Vector3.Lerp(transform.position, itemPos, smoothSpeed);
+        transform.position = smoothPos;
     }
 }
