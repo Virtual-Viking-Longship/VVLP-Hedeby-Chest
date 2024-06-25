6e2989275fc187192eec846986d12117ed7f88e8
Jane P - IDEALAb <parsonj@carleton.edu>
2024-06-25 11:38:12 -0500

    More HQ meshes- readded text boxes for information.

--- Assets/Scripts/FollowItem.cs
+++ Assets/Scripts/FollowItem.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class FollowItem : MonoBehaviour
 {
-    public Transform item;
+    public MeshRenderer targetObject;
+    private GameObject headset;
     public float smoothSpeed = 0.125f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        headset = GameObject.FindGameObjectWithTag("MainCamera").gameObject;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Vector3 itemPos = item.transform.position + new Vector3(0,0.05f,-0.05f);
+        Vector3 offset = (this.transform.position - headset.transform.position).normalized;
+        Vector3 itemPos = targetObject.bounds.center + new Vector3(0, 0.1f, 0f) + offset * .1f;
         Vector3 smoothPos = Vector3.Lerp(transform.position, itemPos, smoothSpeed);
         transform.position = smoothPos;
+        transform.rotation = Quaternion.LookRotation(offset);
     }
 }
